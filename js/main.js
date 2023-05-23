061bc7aa16a82838ab2a7ca0a1827e77dc0995df
PauuBorja <128828354+PauuBorja@users.noreply.github.com>
2023-05-23 09:09:55 -0600

    Operación con números

---
+++ js/main.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+//alert("Bienvenido a JavaScript");
+/*
+comentario multilinea
+*/
+
+//let nombre = prompt("Cual es tu nombre")
+//console.log(parseInt(nombre))
+//console.log(typeof(nombre))
+
+let numeroUno = prompt("Ingresar un numero");
+let numeroDos = prompt("Ingresar otro numero");
+
+let resultadoSuma = parseInt(numeroUno) + parseInt(numeroDos);
+let resultadoResta = parseInt(numeroUno) - parseInt(numeroDos);
+let resultadoMultiplicacion = parseInt(numeroUno) * parseInt(numeroDos);
+let resultadoDivision = parseInt(numeroUno) / parseInt(numeroDos);
+console.log(`El resultado de la suma es:${resultadoSuma}`);
+
